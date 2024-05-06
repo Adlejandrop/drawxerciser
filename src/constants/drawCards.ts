e8abfa8cc0d106f63b7c9a9150d9ed8d719405db
Alejandro Pinedo <apinedoar@gmail.com>
2024-05-06 14:25:35 -0400

    mas ejercicios

--- src/constants/drawCards.ts
+++ src/constants/drawCards.ts
@@ -3,10 +3,25 @@ const drawCards = [
     {"name": "sin levantar", "description": "dibuja sin levantar el lapiz"},
     {"name": "puntillismo", "description": "dibuja solo haciendo puntos con el lapiz"},
     {"name": "mano no habil", "description": "dibuja con tu mano no hábil"},
-    {"name": "cirulos", "description": "dibuja solo cirulos"},
+    {"name": "circulos", "description": "dibuja solo circulos"},
     {"name": "linea liviana", "description": "dibuja presionando el lápiz lo menos posible"},
     {"name": "linea pesada", "description": "dibuja presionando el lapiz lo más posible"},
-    {"name": "chiquito", "description": "dibuja en un espacio pequeño de la hoja"}
+    {"name": "chiquito", "description": "dibuja en un espacio pequeño de la hoja"},
+    {"name": "trazos aleatorios", "description": "realiza trazos rápidos y aleatorios para llenar una sección del papel"},
+    {"name": "contornos sueltos", "description": "dibuja contornos rápidos y no detallados de cualquier objeto a tu alrededor"},
+    {"name": "repetición de formas", "description": "elige una forma simple y repítela en diversas orientaciones y tamaños"},
+    {"name": "hachurado", "description": "utiliza líneas paralelas para crear sombras y texturas rápidas"},
+    {"name": "de afuera hacia adentro", "description": "dibuja desde las esquinas y continua dibujando hasta el centro. puedes repetir el proceso"},
+    {"name": "ondulaciones", "description": "dibuja solo con formas onduladas"},
+    {"name": "a ciegas", "description": "dibuja con los ojos cerrados"},
+    {"name": "un solo ojo", "description": "dibuja con un ojo cerrado"},
+    {"name": "rayones", "description": "raya de manera caótica y veloz"},
+    {"name": "recorrido", "description": "dibuja recorriendo una linea ya hecha. si no hay líneas, crea una rapido y recorrela."},
+    {"name": "maximo detalle", "description": "dibuja en maximo detalle una textura o forma de tu alrededor"},
+    {"name": "tortuga", "description": "dibuja lo más lento que puedas"},
+    {"name": "liebre", "description": "dibuja lo más rápido que puedas"},
+
+    
 ]
 
 export default drawCards
